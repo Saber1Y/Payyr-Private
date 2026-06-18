683636c5c6f7f5c75b7e0c1d6d686bbe37be9f24
Saber1Y <ayodejiakintobi1@gmail.com>
2026-06-18 16:22:40 +0100

    build: force dynamic authenticated layout

--- frontend/app/(authenticated)/layout.tsx
+++ frontend/app/(authenticated)/layout.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+export const dynamic = "force-dynamic";
+
 import { Sidebar } from "@/components/sidebar";
 import { Navbar } from "@/components/navbar";
 import { usePrivy } from "@privy-io/react-auth";
